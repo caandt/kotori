2fa8d8e6926cf436b65a66722ca898431c16ad24
caandt <caandt@twoha.cc>
2026-08-16 18:01:40 -0500

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,116 @@
+{
+  description = "Binary rewriter for AArch64";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    picinae = {
+      type = "github";
+      owner = "CharlesAverill";
+      repo = "Picinae";
+      ref = "duneify";
+      flake = false;
+    };
+    rocq-primitive = {
+      type = "github";
+      owner = "peregrine-project";
+      repo = "rocq-primitive";
+      ref = "8.20.0";
+      flake = false;
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        coqPackages = pkgs.coqPackages_8_20;
+        coq = coqPackages.coq;
+        ocamlPackages = pkgs.ocamlPackages;
+        rocq-picinae = ocamlPackages.buildDunePackage {
+          pname = "rocq-picinae";
+          version = "0.0.0";
+          src = inputs.picinae;
+          nativeBuildInputs = [coq];
+          buildInputs = [coqPackages.stdlib];
+          postInstall = ''
+            mkdir -p $out/lib/coq/${coq.coq-version}
+            ln -s $out/lib/ocaml/*/site-lib/coq/user-contrib \
+              $out/lib/coq/${coq.coq-version}/user-contrib
+          '';
+          env.OCAMLPATH = "${coq}/lib";
+        };
+        rocq-primitive = ocamlPackages.buildDunePackage {
+          pname = "coq-primitive";
+          version = "8.20.0";
+          src = inputs.rocq-primitive;
+        };
+        a64-cc = let
+          cc = pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc;
+        in
+          pkgs.symlinkJoin {
+            name = "a64-cc";
+            paths = [cc];
+            postBuild = ''
+              mkdir -p $out/bin
+              for file in ${cc}/bin/*; do
+                filename=$(basename "$file")
+                ln -sf "$file" "$out/bin/a64-''\${filename#aarch64-unknown-linux-gnu-}"
+              done
+            '';
+          };
+        a8 = ocamlPackages.buildDunePackage {
+          pname = "a8";
+          version = "0.0.0";
+          src = ./.;
+          nativeBuildInputs = [coq a64-cc];
+          buildInputs =
+            [pkgs.lief]
+            ++ (with ocamlPackages; [
+              rocq-primitive
+              ctypes
+              ctypes-foreign
+              cmdliner
+              ppx_deriving
+              ppx_import
+              parmap
+              yojson
+            ])
+            ++ (with coqPackages; [
+              stdlib
+              stdpp
+              rocq-picinae
+              coq-record-update
+              coqutil
+            ]);
+          passthru = {
+            inherit ocamlPackages;
+          };
+          env.OCAMLPATH = "${coq}/lib";
+        };
+      in {
+        default = a8;
+      }
+    );
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        a8 = self.packages.${system}.default;
+      in {
+        default = pkgs.mkShell {
+          inputsFrom = [a8];
+          packages = [
+            a8.ocamlPackages.utop
+            pkgs.perf
+          ];
+        };
+      }
+    );
+  };
+}
